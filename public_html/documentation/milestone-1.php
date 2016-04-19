a9a097f26a5ebf373a4030803dc02e301df68b94
Elliot Murrey <elliotmurrey@gmail.com>
2016-04-19 01:03:57 -0600

    adding persona

--- public_html/documentation/milestone-1.php
+++ public_html/documentation/milestone-1.php
@@ -7,5 +7,19 @@
 			<p><strong>Purpose:</strong> to help showcase my skills as a developer and give possible employers a method of contact</p>
 			<p><strong>Audience:</strong> potential employers, and clients.</p>
 			<p><strong>Goal:</strong> to acquire a well paying full time job, and potential clients</p>
+			<h1>Persona</h1>
+				<p>Personal</p>
+					<ul>
+						<li>Name: Scott Wiggins</li>
+						<li>Gender: Male</li>
+						<li>Education: BA in business</li>
+						<li>Behaviors: has been around business for a while and doesn't like to play, feels as though time is money</li>
+						<li>Favorite quotes: "time is money." & insanity is doing the same thing over and over again expecting different results.</li>
+					</ul>
+				<p>Professional</p>
+					<ul>
+						<li>Role: ceo of his company SPAR</li>
+						<li>Background: interned for google after getting BA, later going on to work for Amazon for nearly 20 year.</li>
+					</ul>
 		</body>
 </html>
