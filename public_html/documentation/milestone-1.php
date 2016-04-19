f2158fdb24e32db836e90aa3bcf6e7ac254aefb5
Elliot Murrey <elliotmurrey@gmail.com>
2016-04-19 00:43:02 -0600

    added purpose, audience, and goal

---
+++ public_html/documentation/milestone-1.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+	<html>
+		<head>
+			<title> Elliot Murrey web developer</title>
+		</head>
+		<body>
+			<p><strong>Purpose:</strong> to help showcase my skills as a developer and give possible employers a method of contact</p>
+			<p><strong>Audience:</strong> potential employers, and clients.</p>
+			<p><strong>Goal:</strong> to acquire a well paying full time job, and potential clients</p>
+		</body>
+</html>
