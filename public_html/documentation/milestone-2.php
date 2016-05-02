6d4d029827a3d9428ad2dd387a10b34f9d8beb67
Elliot Murrey <elliotmurrey@gmail.com>
2016-05-02 05:03:47 -0600

    trying to display miles stone 2

---
+++ public_html/documentation/milestone-2.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="UTF-8">
+		<title>emurrey milestone-2</title>
+	</head>
+	<body>
+		<h2>Sitemap</h2>
+		<img src="pwpsitemap.svg" alt="sitemap.svg">
+	</body>
+</html>
