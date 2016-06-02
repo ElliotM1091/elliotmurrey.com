7ff7356d111ca2f7732c56a6e7dc2a6a980ecf9f
Elliot Murrey <elliotmurrey@gmail.com>
2016-06-02 15:14:12 -0600

    mile stone 2a finished?

--- public_html/php/pages/homePage.php
+++ public_html/php/pages/homePage.php
@@ -27,4 +27,13 @@
 		<!-- Latest compiled and minified JavaScript -->
 		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
 	</head>
+	<body class="sfooter">
+		<div class="sfooter">
+
+			<!-- header-->
+			<header class="p-y-4">
+				
+			</header>
+		</div>
+	</body>
 </html>
