e6db00292cf4e40932c811b92a718c0139f3a191
Elliot Murrey <elliotmurrey@gmail.com>
2016-06-04 18:27:15 -0600

    mile stone 2a finished?

--- public_html/index.php
+++ public_html/index.php
@@ -5,27 +5,31 @@
 		<!--this is hte 8 bit setting used universally. this is a self closing tag-->
 		<meta charset="utf-8">
 		<!--this helps out IE-->
-		<meta http-equiv="X-UA-Compatible" content="IE=edge" />
+		<meta http-equiv="X-UA-Compatible" content="IE=edge"/>
 		<!--going to be used for view port -->
-		<meta name="viewport" content="width=device-width, initial-scale=1" />
+		<meta name="viewport" content="width=device-width, initial-scale=1"/>
 
 		<!-- Latest compiled and minified CSS -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css" integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
+		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap.min.css"
+				integrity="sha384-1q8mTJOASx8j1Au+a5WDVnPi2lkFfwwEAa8hDDdjZlpLegxhjVME1fgjWPGmkzs7" crossorigin="anonymous">
 
-		<!-- Optional theme -->
-		<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/css/bootstrap-theme.min.css" integrity="sha384-fLW2N01lMqjakBkx3l/M9EahuwpSfeNvV63J5ezn3uZzapT0u7EYsXMjQV+0En5r" crossorigin="anonymous">
 
 		<!-- Font Awesome -->
-		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css" rel="stylesheet" />
+		<link type="text/css" href="//maxcdn.bootstrapcdn.com/font-awesome/4.6.2/css/font-awesome.min.css"
+				rel="stylesheet"/>
 
 		<!--customer CSS-->
 		<link rel="stylesheet" href="./css/styles.css" type="text/css">
+		<!-- favicon -->
+		<link rel="shortcut icon" href="./image/favicon.ico" type="image/x-icon" />
 
 		<!-- jQuery (needed for Bootstrap's JavaScript plugins) -->
 		<script src="https://ajax.googleapis.com/ajax/libs/jquery/2.2.4/jquery.min.js"></script>
 
 		<!-- Latest compiled and minified JavaScript -->
-		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js" integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS" crossorigin="anonymous"></script>
+		<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.6/js/bootstrap.min.js"
+				  integrity="sha384-0mSbJDEHialfmuBBQP6A4Qrprq5OVfW37PRR3j5ELqxss1yVqOtnepnHVP9aJ7xS"
+				  crossorigin="anonymous"></script>
 	</head>
 	<body class="sfooter">
 		<div class="sfooter">
@@ -33,63 +37,78 @@
 			<!-- header-->
 			<header class="p-y-4">
 				<div class="container">
-					<nav class="navbar navbar-default">
-						<div class="container-fluid">
-							<!-- Brand and toggle get grouped for better mobile display -->
-							<div class="navbar-header">
-								<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-									<span class="sr-only">Toggle navigation</span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-									<span class="icon-bar"></span>
-								</button>
-								<a href="#" class="thumbnail">
-									<img src="image/20141112_161628-1.png" alt= "pic of me" width="200" height="280">
-								</a>
-							</div>
 
-							<!-- Collect the nav links, forms, and other content for toggling -->
-							<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-								<ul class="nav navbar-nav">
-									<li class="active"><a href="#">ELLIOT MURREY <span class="sr-only">(current)</span></a></li>
+					<div class="row">
+						<div class="col-md-3">
+							</button>
+							<a href="#" class="thumbnail">
+								<img src="image/20141112_161628-1.png" alt="pic of me" width="200" height="280">
+							</a>
+						</div>
+						<div class="col-md-9">
+							<! --
+							<div class="block_1"><h1 class="my-name">Elliot Murrey</h1></div>
+							<hr/>
+
+							<div class="container-fluid">
+
+
+								<li class="active"><a href="#"> <span class="sr-only">(current)</span></a>
+								</li>
 
 								</ul>
 								<ul class="nav navbar-nav navbar-right">
-									<li><a href="#">About me</a></li>
-									<li><a href="#">Portfolio</a></li>
-									<li><a href="#">Contact me</a></li>
+									<li class="list"><a href="#">About me</a></li>
+									<li class="list"><a href="#">Portfolio</a></li>
+									<li class="list"><a href="#">Contact me</a></li>
 								</ul>
-							</div><!-- /.navbar-collapse -->
-						</div><!-- /.container-fluid -->
-					</nav>
-				</div>
-			</header>
+							</div>
+							<div class="block_1"></div>
+							<hr/>
+						</div>
 
-			<!-- welcome!-->
-			<!-- welcome section -->
-			<section id="welcome" class="p-y-4">
-				<div class="container">
-					<div class="row">
-						<div class="col-md-9">
-							<div class="jumbotron">
-								<h1>Hello!</h1>
-								<p>My Name is Elliot Murrey. I am a junior web developer</p>
+					</div>
+
+				</div>
+		</div>
+		</header>
+		<!-- welcome!-->
+		<!-- welcome section -->
+		<h1></h1>
+		<h1 class="about-me">ABOUT ME</h1>
+		<section id="welcome" class="p-y-4">
+			<div class="container">
+				<div class="row row-flex">
+					<div class="col-md-8">
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h2 class="panel-title">Here are some of the dets</h2>
+							</div>
+							<div class="panel-body">
+								<p>My Name is Elliot Murrey. I am a full stack junior web developer based in Albuquerque New Mexico. I enjoy creating websites and absolutely love programming.</p>
+								<p>I love working with people and would love to help you create your dream. Rather it be a personal website, or taking your dream or a thought and making it a reality. I'm the guy for you.  </p>
 							</div>
 						</div>
-						<div class="col-md-3">
-							<div class="">
-								<h2>Programming skills!</h2>
+					</div>
+
+					<div class="col-md-4">
+						<div class="panel panel-default">
+							<div class="panel-heading">
+								<h3 class="panel-title">Tech Tool box!</h3>
+								</div>
 								<ul>
 									<li>PHP</li>
 									<li>mySQL</li>
 									<li>javaScript</li>
+									<li>CSS</li>
+									<li>html</li>
 								</ul>
-							</div>
 						</div>
 					</div>
 				</div>
+			</div>
 
-			</section>
+		</section>
 		</div>
 	</body>
 </html>
